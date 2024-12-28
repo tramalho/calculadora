0493f838859e269f3d0f48f24e40621c9d9737f0
tramalho <thiagofacul@gmail.com>
2024-12-28 07:19:57 -0300

    Componente Calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+export default class Calculator extends Component {
+	render() {
+		return (
+			<>
+				<h1>Calculator</h1>
+				<div className="calculator" />;
+			</>
+		);
+	}
+}
