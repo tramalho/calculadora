5e56762bc266fa1ab4fc6f09aa3b84275998722d
tramalho <thiagofacul@gmail.com>
2024-12-28 07:44:23 -0300

    Componente Button #01

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.css";
+
+export default function Button(props) {
+	return (
+		<>
+			<button className="button" type="button">
+				{props.label}
+			</button>
+		</>
+	);
+}
